fc5fcf2da620c167c07ad58858a106162361edf5
ruslan <ruslankrm@bigmir.net>
2018-10-08 13:09:43 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-function numberOperClick(e) {
-    document.querySelectorAll('.number, .knopka')
-        .forEach(el => el.addEventListener('click', numberOperClick));
-    const target = e.target;
-    const display = document.querySelector('.display');
-    display.value += target.innerText;
-}
-
-document.querySelector('.equal')
-addEventListener('click', equal);
-
-function equal() {
-    const display = document.querySelector('.display');
-    display.value = eval(display.value);
-}
+document.querySelectorAll('.number, .knopka')
+    .forEach( el => el.addEventListener('click', digitOperClick));
+    
+    function digitOperClick(e) {
+        const target = e.target;
+        const display = document.querySelector('.display');
+        display.value += target.innerText;
+    }
+    
+    document.querySelector('.equal')
+    .addEventListener('click', equal);
+    
+    function equal() {
+        const display = document.querySelector('.display');
+        display.value = eval(display.value)
+    }
